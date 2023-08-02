d77b35e92f1a5c5e1143c66f7f8ee197bbc5d299
ameryakubov <alex_meryakubov@mail.ru>
2023-08-02 21:58:58 +0300

    include DragAndDropTest

---
+++ src/test/java/DragAndDropTest.java
@@ -0,0 +1,34 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragAndDropTest {
+    static {
+        ChromeOptions options = new ChromeOptions();
+        options.setBinary("/Applications/Google Chrome.app/Contents/MacOS/Google Chrome");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+        Configuration.browserCapabilities = capabilities;
+    }
+
+
+    @BeforeAll
+    static public void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void changeColumnTest() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo($("#column-b"));
+        $("#column-a").shouldHave(Condition.text("b"));
+        $("#column-b").shouldHave(Condition.text("a"));
+
+    }
+}
